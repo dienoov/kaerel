e7f94b3cffccc1d5ca90f6def57588a7ba304d03
dienoov <alvinodien@yahoo.co.id>
2023-12-22 10:34:45 +0700

    Add fonts

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'Plus Jakarta Sans'
+      ),
       home: HomePage(),
     );
   }
