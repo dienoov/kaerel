317a1458c6211a6d8505d9a8ebc68dfa5f3efacd
dienoov <alvinodien@yahoo.co.id>
2023-12-22 11:08:36 +0700

    Change color theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Plus Jakarta Sans'
+        fontFamily: 'Plus Jakarta Sans',
+        primarySwatch: Colors.indigo,
       ),
       home: HomePage(),
     );
